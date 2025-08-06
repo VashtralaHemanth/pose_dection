e7ff9b47df5ac40d69302a9d38c6c50b12058775
VashtralaHemanth <hemanthv748@gmail.com>
2025-08-07 02:05:12 +0530

    Update pose_dection.py

--- pose_dection.py
+++ pose_dection.py
@@ -73,3 +73,74 @@ def detectPose(image, pose, display=True):
         
         # Return the output image and the found landmarks.
         return output_image, landmarks
+  pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
+
+# Initialize the VideoCapture object to read from the webcam.
+#video = cv2.VideoCapture(0)
+
+# Initialize the VideoCapture object to read from a video stored in the disk.
+video = cv2.VideoCapture('/content/pose2.mp4')
+
+
+# Initialize a variable to store the time of the previous frame.
+time1 = 0
+
+# Iterate until the video is accessed successfully.
+while video.isOpened():
+    
+    # Read a frame.
+    ok, frame = video.read()
+    
+    # Check if frame is not read properly.
+    if not ok:
+        
+        # Break the loop.
+        break
+    
+    # Flip the frame horizontally for natural (selfie-view) visualization.
+    frame = cv2.flip(frame, 1)
+    
+    # Get the width and height of the frame
+    frame_height, frame_width, _ =  frame.shape
+    
+    # Resize the frame while keeping the aspect ratio.
+    frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
+    
+    # Perform Pose landmark detection.
+    frame, _ = detectPose(frame, pose_video, display=False)
+    
+    # Set the time for this frame to the current time.
+    time2 = time()
+    
+    # Check if the difference between the previous and this frame time > 0 to avoid division by zero.
+    if (time2 - time1) > 0:
+    
+        # Calculate the number of frames per second.
+        frames_per_second = 1.0 / (time2 - time1)
+        
+        # Write the calculated number of frames per second on the frame. 
+        cv2.putText(frame, 'FPS: {}'.format(int(frames_per_second)), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
+    
+    # Update the previous frame time to this frame time.
+    # As this frame will become previous frame in next iteration.
+    time1 = time2
+    
+    # Display the frame.
+    cv2_imshow(frame)
+
+    
+    # Wait until a key is pressed.
+    # Retreive the ASCII code of the key pressed
+    k = cv2.waitKey(1) & 0xFF
+    
+    # Check if 'ESC' is pressed.
+    if(k == 27):
+        
+        # Break the loop.
+        break
+
+# Release the VideoCapture object.
+video.release()
+
+# Close the windows.
+cv2.destroyAllWindows()
